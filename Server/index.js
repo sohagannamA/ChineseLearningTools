d27f7d1ab6aa4bd65ad52d3e91ccd8881c73994b
The Blackened Soul <113580160+sohagannamA@users.noreply.github.com>
2025-03-24 03:31:02 +0600

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Router = require("./Routes/route")
 const dotenv = require("dotenv");
 const cors = require("cors");
+
 const Dbconnect = require("./Config/config");
 dotenv.config();
 const PORT = process.env.PORT || 8000
@@ -11,7 +12,9 @@ const HOSTNAME = process.env.LOCALHOST;
 const app =express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', // সব জায়গা থেকে রিকুয়েস্ট গ্রহণ করবে
+}));
 app.use(Router);
 app.listen(PORT,HOSTNAME,()=>{
     console.log(`Your server is running at: http://${HOSTNAME}:${PORT}`)
