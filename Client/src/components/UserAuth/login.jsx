a970d52dd13a678f1a2fc109f42f68973bff68f3
The Blackened Soul <113580160+sohagannamA@users.noreply.github.com>
2025-03-24 03:09:27 +0600

    Update login.jsx

--- Client/src/components/UserAuth/login.jsx
+++ Client/src/components/UserAuth/login.jsx
@@ -17,7 +17,7 @@ export default function Login({setToken}) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("http://127.0.0.1:8000/login", {
+            const response = await axios.post("https://chineselearningtools-2.onrender.com/login", {
                 email,
                 password
             });
